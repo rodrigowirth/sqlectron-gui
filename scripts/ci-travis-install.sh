62e0b4d853dc077382a57ab678d1cd325e581ba7
Max Claus Nunes <maxcnunes@gmail.com>
2016-04-18 22:44:42 -0300

    Install docker manually

--- scripts/ci-travis-install.sh
+++ scripts/ci-travis-install.sh
@@ -18,6 +18,9 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   DOCKER_COMPOSE_VERSION="1.5.2"
 
+  sudo apt-get install -y linux-image-virtual kernel linux-image-extra-virtual
+  curl -sSL https://get.docker.com/ | sudo sh
+
   sudo rm /usr/local/bin/docker-compose || echo "No previous docker-compose installed"
   curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
   chmod +x docker-compose
