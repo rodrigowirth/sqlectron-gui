bea392c5681e725805743bea0da2128cf36896a9
Max Claus Nunes <maxcnunes@gmail.com>
2016-04-18 22:52:13 -0300

    Remove kernel package

--- scripts/ci-travis-install.sh
+++ scripts/ci-travis-install.sh
@@ -19,7 +19,7 @@ elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   DOCKER_COMPOSE_VERSION="1.5.2"
 
   sudo apt-get update
-  sudo apt-get install -y linux-image-virtual kernel linux-image-extra-virtual
+  sudo apt-get install -y linux-image-virtual linux-image-extra-virtual
   curl -sSL https://get.docker.com/ | sudo sh
 
   sudo rm /usr/local/bin/docker-compose || echo "No previous docker-compose installed"
